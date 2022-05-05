6a3dbd2850e3946bfc6725b1fdf91812f34d234b
Felipe <fquiceno1@misena.edu.co>
2022-05-05 14:55:49 -0500

    [CREATE] Header,Footer y Layout. En el Layout ponemos los componentes que se mantienen a lo largo de toda la aplicacion

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <div className='Footer'>
+            <p className='Footer-title'>Platzi Conf Merge</p>
+            <p className='Footer-copy'>Todos los izquierdos Reservados</p>
+        </div>
+     );
+}
+ 
+export default Footer;
