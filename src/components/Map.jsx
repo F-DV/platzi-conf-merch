bfaafdbc425b727529a183bb3c12c50e6c50be5a
Felipe <fquiceno1@misena.edu.co>
2022-05-06 12:55:39 -0500

    [UPDATE],custom hook para google maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { GoogleMap, LoadScript,Maker } from '@react-google-maps/api';
-const Map = () => {
+const Map = ({data}) => {
     const mapStyles ={
         height: "50vh",
         width:"100%",
     }
     const defaultCenter = {
-        lat:194267261, lng:-99.1718796
+        lat:data.lat, lng: data.lng
     }
     
     return ( 
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -2,17 +2,20 @@ import React, {useContext} from 'react';
 import '../styles/components/Success.css';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
+
 
 const Success = () => {
   const {state} = useContext(AppContext);
   const {buyer} = state;
+  const loaction = useGoogleAddress(buyer[0].address);
   return (
     <div className="Succes">
       <div className="Success-content">
         <h2>`${buyer.name}, Gracias por tu compra`</h2>
         <span>Tu pedido llegara en 3 dias a tu dirección:</span>
         <div className="Success-map">
-          <Map />
+          <Map  data={location}/>
         </div>
       </div>
     </div>
